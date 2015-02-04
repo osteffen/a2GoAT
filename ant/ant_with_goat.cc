829ba4be3f6197b96624b6fcc58f48660d9f6e74
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-02-04 11:53:02 +0100

    FIX ant_with_goat: not running with -O3
    replaced call to Init() of base class with Init()

--- ant/ant_with_goat.cc
+++ ant/ant_with_goat.cc
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     }
 
     // Perform full initialisation
-    if(!analysis->GConfigFile::Init())
+    if(!analysis->Init(""))
     {
         cout << "ERROR: Init failed!" << endl;
         return 0;

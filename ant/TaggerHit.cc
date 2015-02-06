804d648fd22da5710afa9aa232e75696f41df8e8
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-02-06 17:37:34 +0100

    Added TaggerHit class

---
+++ ant/TaggerHit.cc
@@ -0,0 +1,9 @@
+#include "TaggerHit.h"
+
+using namespace ant;
+using namespace std;
+
+std::ostream &TaggerHit::Print(std::ostream &stream) const
+{
+    stream << "TaggerHit: Channel=" << channel << " PhotonEnergy=" << photon_energy << " MeV  Time=" << time << "ns";
+}
